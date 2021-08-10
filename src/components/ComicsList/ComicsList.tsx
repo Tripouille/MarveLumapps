7874cf6bdfd7c28e2c87b280ea0457a2330a2c2e
Tripouille <jmichel.gambard@gmail.com>
2021-08-10 20:53:01 +0200

    change comicsList display

--- src/components/ComicsList/ComicsList.tsx
+++ src/components/ComicsList/ComicsList.tsx
@@ -7,19 +7,15 @@ interface IProps {
 }
 
 const ComicsList: React.FC<IProps> = ({ title, comics }) => {
-	if (comics?.length) return (
+	const availableComics = comics?.length ? comics?.map((comic, index) => <Comic key={index} comic={comic} />) 
+											: <p id="error-message">No comics available</p>;
+										
+	return (
 		<div id="comics-list">
 			<h2 id="list-title">{title}</h2>
-			{
-				comics.map((comic, index) => <Comic key={index} comic={comic} />)
-			}
+			{availableComics}
 		</div>
-	)
-	else return (
-		<div id="comics-list">
-			<h2 id="list-title">No available Comics</h2>
-		</div>
-	)
+	);
 }
 
 export default ComicsList;
