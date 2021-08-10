973ab56cadabf806227a73c24673d65ec588df7e
Tripouille <jmichel.gambard@gmail.com>
2021-08-10 05:40:31 +0200

    improve searchResults with reducer

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,9 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { CharactersList, Pagination } from '..';
 import { getCharacters } from '../../api/api';
 import { BoxLoading } from 'react-loadingg';
 import { Kind, Message } from '@lumx/react';
+
+enum EStatus {
+	loading = "loading",
+	success = "success",
+	failure = "failure",
+	error = "error"
+}
 
 interface IParams {
 	searchQuery: string;
@@ -15,54 +22,60 @@ interface IProps {
 	detailsPath: string;
 }
 
-enum EStatus {
-	loading = "loading",
-	success = "success",
-	failure = "failure",
-	error = "error"
+interface IInfos {
+	characters: [];
+	availableCharacters: number;
+	status: EStatus;
+}
+
+const reducer = (infos: IInfos , action: {type: string, payload?: {}}): IInfos => {
+	switch (action.type) {
+		case 'update':
+			return ({...infos, ...action.payload});
+		case 'reset':
+			return ({characters: [], availableCharacters: 0, status: EStatus.loading});
+		default:
+			throw (new Error('Invalid action in searchResults reducer.'));
+	}
 }
 
 const SearchResult: React.FC<IProps> = React.memo(({ path, detailsPath }) => {
 	console.log("render SearchResult");
 	const characterPerPage = 4;
 	const { searchQuery, currentPage } = useParams<IParams>();
-	const [characters, setCharacters] = useState([]);
-	const [availableCharacters, setAvailableCharacters] = useState(0);
-	const [status, setStatus] = useState<EStatus>(EStatus.loading);
+	const [infos, setInfos] = useReducer(reducer, {characters: [], availableCharacters: 0, status: EStatus.loading});
 
 	useEffect(() => {
-		setCharacters([]);
-		setAvailableCharacters(0);
-		setStatus(EStatus.loading);
+		if (infos.status !== EStatus.loading)
+			setInfos({type: 'reset'});
 		const loadCharacters = async () => {
 			try {
 				const results = await getCharacters({ nameStartsWith: searchQuery.trim(), orderBy: 'name',
-									offset: (+currentPage - 1) * characterPerPage, limit: characterPerPage});
-				console.table(results);
-				setCharacters(results.data);
-				setAvailableCharacters(results.data.length > 0 ? results.total : 0);
-				setStatus(results.data.length > 0 ? EStatus.success : EStatus.failure);
-			} catch {
-				setStatus(EStatus.error);
-			}
-		};
-		loadCharacters();
+				offset: (+currentPage - 1) * characterPerPage, limit: characterPerPage});
+				setInfos({type: 'update', payload: {characters: results.data,
+					availableCharacters: results.data.length > 0 ? results.total : 0,
+					status: results.data.length > 0 ? EStatus.success : EStatus.failure}});
+				} catch {
+					setInfos({type: 'update', payload: {status: EStatus.error}});
+				}
+			};
+			loadCharacters();
+	// eslint-disable-next-line
 	}, [searchQuery, currentPage]);
 
-	
-	if (status === EStatus.loading) return <BoxLoading  color="#0000ff" speed={0.3} size="large" />
-	else if (status === EStatus.success) return (
+	if (infos.status === EStatus.loading) return <BoxLoading color="#0000ff" speed={0.3} size="large" />
+	else if (infos.status === EStatus.success) return (
 		<section className="lumx-spacing-padding-horizontal-huge">
-			<CharactersList characters={characters} detailsPath={detailsPath} />
-			<Pagination itemPerPage={characterPerPage} itemNumber={availableCharacters} path={`${path}/${searchQuery}`} />
+			<CharactersList characters={infos.characters} detailsPath={detailsPath} />
+			<Pagination itemPerPage={characterPerPage} itemNumber={infos.availableCharacters} path={`${path}/${searchQuery}`} />
 		</section>
 	);
-	else if (status === EStatus.failure) return (
+	else if (infos.status === EStatus.failure) return (
 		<Message id='error-message' kind={Kind.warning} hasBackground>
 			<p>No results available soory :(</p>
 		</Message>
 	);
-	else if (status === EStatus.error) return (
+	else if (infos.status === EStatus.error) return (
 		<Message id='error-message' kind={Kind.error} hasBackground>
 			<p>Oops something went wrong.</p>
 		</Message>
