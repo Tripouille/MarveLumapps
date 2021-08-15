8e696c5a8b15cbbe4b2428dca8ecf04e67e9c25f
Jean-Michel <jmichel.gambard@gmail.com>
2021-08-15 02:40:31 +0200

    clean Character (use Thumbnail from design system)

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlexBox, Alignment, Button } from "@lumx/react";
+import { FlexBox, Alignment, Button, Thumbnail, AspectRatio } from "@lumx/react";
 import { Link } from "react-router-dom";
 import { ICharacter } from "../../Utils/types";
 
@@ -11,17 +11,13 @@ interface IProps {
 const Character: React.FC<IProps> = ({ character, path }) => {
   return (
     <FlexBox className="character-flexbox" vAlign={Alignment.left}>
-      <img
-        className="character-image"
-        src={character.image}
-        alt={character.name}
-      ></img>
+      <Thumbnail className="character-image" image={character.image} alt={character.name} aspectRatio={AspectRatio.original} />
       <FlexBox className="character-infos-flexbox" vAlign={Alignment.left}>
-        <Link className="link" to={path}>
+        <Link aria-label="See details" to={path}>
           <h1 className="character-name">{character.name}</h1>
         </Link>
         <div className="character-description">{character.description}</div>
-        <Link className="link" to={path}>
+        <Link to={path}>
           <Button className="see-details-button">See details</Button>
         </Link>
       </FlexBox>
